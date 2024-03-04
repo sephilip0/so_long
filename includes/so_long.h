f794a5f57f9b668f04b2dd7f377dcce94af3e682
Sergey Philippov <sephilip@c2r6s4.42lisboa.com>
2024-03-04 14:20:52 +0000

    4.03

--- includes/so_long.h
+++ includes/so_long.h
@@ -33,8 +33,6 @@ typedef struct s_frame
 	int			player_y;
 	int			player_state_x;
 	int			player_state_y;
-	int			movement_count;
-	int			current_asset;
 	t_image		front;
 	t_image		frame;
 }	t_frame;
@@ -42,17 +40,14 @@ typedef struct s_frame
 typedef struct s_map
 {
 	char	**map;
-//	char	**collisions;
-//	char	*collect_str;
 	int		collect;
 	int		height;
 	int		width;
 	int		player_y;
 	int		player_x;
 	int		idle;
+	int		walk;
 	int		active_exit;
-//	int		player_state_x;
-//	int		player_state_y;
 	int		exit_y;
 	int		exit_x;
 }	t_map;
@@ -61,28 +56,20 @@ typedef struct s_mlx
 {
 	void		*mlx;
 	void		*window;
-//	t_frame		*frame;
 	t_map		*map;
-//mine
 	t_image		frame;
 	t_image		*asset;
 	int		dif_timer;
-	long		frame_time;
 	int			steps;
-	int			step_bol;
-	char		*steps_str;
 	int			s;
-	int			mov;
-	int			x;
-	int			y;
 }	t_mlx;
 
 //MAIN.C
 void	map_count_row(t_map *map, char *file);
-void	free_map(char **map);
+void	free_map(char **map, int ret);
 void	init_map(t_map *map, char *file);
 void	flood_fill(t_map *map, int y, int x, int *c);
-int	map_param(t_map *map);
+void	map_param(t_map *map);
 void	check_map(t_map *map);
 void	map_constructor(t_map *map, char *file);
 void	ft_putmat(char **mat);
@@ -94,8 +81,8 @@ t_image	*init_assets(t_mlx *root);
 unsigned int	get_pixel_color(t_image *img, int x, int y);
 void	put_pixel(t_image *img, int x, int y, int color);
 void	draw_part(t_mlx *root, t_image *image, int x, int y);
-void	draw_map(t_mlx *root);
-void	movement_player(t_mlx *root, int movx, int movy);
+void	draw_map(t_mlx *root, int ch);
+void	movement_player(t_mlx *root, int movx, int movy, int lr);
 int	input_player(int keysym, t_mlx *root);
 int	looper(t_mlx *root);
 
